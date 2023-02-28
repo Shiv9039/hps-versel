1feb24b77ba8276be11144f117705f1d5e1f3839
Shivendra <spatel@nabhtech.com>
2023-03-01 00:48:43 +0530

    asd

--- health_prediction_system/settings.py
+++ health_prediction_system/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT =  BASE_DIR / 'static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT =  BASE_DIR / 'static/'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os. path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR / 'media/'
